2bf52e27ce93ef949a7d25600dfe28046f410cea
Mariya Mego <31904600+mash-a@users.noreply.github.com>
2021-07-08 17:43:41 -0700

    Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import { terser } from "rollup-plugin-terser";
-import copy from 'rollup-plugin-copy'
-import { version } from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import babel from '@rollup/plugin-babel';
+import { version, main, module } from './package.json';
 
 const banner = `/*!
  * jquery-timepicker v${version} - A jQuery timepicker plugin inspired by Google Calendar. It supports both mouse and keyboard navigation.
@@ -10,26 +11,23 @@ const banner = `/*!
  */`;
 
 export default {
-  input: 'src/jquery.timepicker.js',
+  input: 'src/index.js',
   output: [
     {
-      file: 'jquery.timepicker.js',
-      format: 'iife',
-      banner: banner
+      file: main,
+      format: 'cjs',
+      sourcemap: true
     },
     {
-      file: 'jquery.timepicker.min.js',
-      format: 'iife',
-      plugins: [terser()]
+      file: module,
+      format: 'esm',
+      sourcemap: true,
     }
   ],
   plugins: [
-    copy({
-      targets: [
-        { src: 'src/static/jquery.timepicker.css', dest: './' },
-        { src: 'src/static/jquery.timepicker.css', dest: './', rename: 'jquery.timepicker.min.css' },
-      ]
-    }),
-    babel({ babelHelpers: 'bundled' })
+    babel({ babelHelpers: 'bundled' }),
+    terser(),
+    resolve(),
+    commonjs(),
   ]
 };

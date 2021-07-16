0c742cd5e4e81c5b1731b34687edfb1d5654fbba
Mariya Mego <31904600+mash-a@users.noreply.github.com>
2021-07-16 16:19:27 -0700

    Add scroll to rounded value

--- src/Components/ListItem/ListItem.js
+++ src/Components/ListItem/ListItem.js
@@ -1,12 +1,41 @@
 import * as React from 'react';
+import { classNames } from 'utils/constants';
 
-const ListItem = ({ meridiemClass, selectedClass, children, setTimeValue, value, label }) => {
+const ListItem = ({
+  children,
+  label,
+  meridiemClass,
+  roundedValue,
+  setTimeValue,
+  value,
+}) => {
+  const ref = React.useRef(null);
+  const [selected, setSelected] = React.useState('');
+
+  React.useEffect(() => {
+    if (roundedValue === value) {
+      setSelected(classNames.selected);
+      setTimeout(() => {
+        ref.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'start',
+      });
+      }, 0);
+    } else {
+      setSelected('');
+    }
+  }, [roundedValue, value]);
+
   const handleClick = () => {
     setTimeValue(label);
   };
 
   return (
-    <li className={`${meridiemClass} ${selectedClass}`} onClick={handleClick}>
+    <li
+      ref={ref}
+      className={`${meridiemClass} ${selected}`}
+      onClick={handleClick}>
       {children}
     </li>
   );

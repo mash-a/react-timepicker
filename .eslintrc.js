7707e06538cdf27be36c151f5b90dcdf71877f77
Mariya Mego <31904600+mash-a@users.noreply.github.com>
2021-07-15 17:42:53 -0700

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     allowImportExportEverywhere: true,
     ecmaFeatures: {

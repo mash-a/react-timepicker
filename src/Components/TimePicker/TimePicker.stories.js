6dfcb77a8fb5d501b5a4410fb785e13dbd41ecf3
Mariya Mego <31904600+mash-a@users.noreply.github.com>
2021-07-28 11:52:10 -0700

    Add more timepickers to storybook

--- src/Components/TimePicker/TimePicker.stories.js
+++ src/Components/TimePicker/TimePicker.stories.js
@@ -5,4 +5,17 @@ export default {
   title: 'TimePicker',
 };
 
-export const TestTimePicker = () => <TimePicker />;
+export const TestTimePicker = () => {
+  const handleChange = value => console.log({ value });
+  return (
+    <div>
+      <TimePicker onChange={handleChange} value={'9:00:00 AM'}/>
+      <TimePicker onChange={handleChange} value={'dfgfdg'}/>
+      <TimePicker onChange={handleChange} value={'15:32 PM'} options={{ step: 30 }}/>
+      <TimePicker onChange={handleChange} value={''}/>
+      <TimePicker onChange={handleChange} value={'01:12 AM'} options={{ disableTextInput: true }}/>
+    </div>
+
+  );
+
+};
